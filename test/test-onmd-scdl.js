6b898dfe1ac335896d363066a84d1243c7c20add
Chris Russell <chris@azuqua.com>
2014-11-20 17:32:19 -0800

    Update to latest onm v0.3.6

--- test/test-onmd-scdl.js
+++ test/test-onmd-scdl.js
@@ -5,7 +5,7 @@ var expect = chai.expect;
 var should = chai.should;
 
 var SCDLDataModelDeclaration = require('../index').dataModelDeclaration;
-var validateDataModelDeclaration = require('onm-data-model-tests')().validateDataModelDeclaration;
+var validateDataModelDeclaration = require('onm-data-model-tests').validateDataModelDeclaration;
 
 describe("onmd-scdl module tests", function() {
     validateDataModelDeclaration(SCDLDataModelDeclaration);
